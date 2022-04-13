16982a67b1e19759127506545ec2b0484cdd3367
reperezc <20200281@esen.edu.sv>
2022-04-12 20:45:48 -0600

    pura kk

--- routes/orderdetails.py
+++ routes/orderdetails.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user, login_required
-from forms.orderDetailCreateForm import OrderDetailCreateForm
+from forms.saleDetailCreateForm import SaleCreateform
 from utils.db import db
 from models.order import Order
-from models.orderdetail import OrderDetail
+from models.orderdetail import Sale
 
 orderDetails = Blueprint("orderDetails", __name__, url_prefix="/orderDetails")
 
@@ -11,7 +11,7 @@ orderDetails = Blueprint("orderDetails", __name__, url_prefix="/orderDetails")
 @orderDetails.route("/<int:id_order>")
 @login_required
 def home(id_order):
-    details = OrderDetail.query.filter_by(orderId = id_order)
+    details = Sale.query.filter_by(orderId = id_order)
     return render_template("orders/ordersDetails.html", user=current_user, orders=details)
 
 
@@ -19,16 +19,16 @@ def home(id_order):
 @login_required
 def homeByOrderId(orderId):
     currentOrder = Order.query.filter_by(id=orderId).first()
-    orderDetailList = OrderDetail.query.filter_by(orderId=orderId).all()
+    orderDetailList = Sale.query.filter_by(orderId=orderId).all()
     return render_template("orderDetails/home.html", order=currentOrder, user=current_user, items=orderDetailList)
 
 
 @orderDetails.route("/create/<int:orderId>", methods=["GET", "POST"])
 @login_required
 def create(orderId):
-    form = OrderDetailCreateForm()
+    form = SaleCreateform()
     if form.validate_on_submit():
-        currentOrder = Order.query.filter_by(id=orderId).first()
+        currentSale = Sale.query.filter_by(id=orderId).first()
 
         quantity = form.quantity.data
         description = form.description.data
